6618699b3972d9ad15ec9960ce478f13d087b992
rasool <rasools27@gmail.com>
2018-08-17 13:57:42 +0530

    maven project mv

---
+++ src/main/java/richie/App.java
@@ -0,0 +1,7 @@
+package richie;
+
+public class App {
+public String sample() {
+return "Sample Program";
+}
+}
---
+++ src/test/java/richie/AppTest.java
@@ -0,0 +1,13 @@
+package richie;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+	}
+
+}
